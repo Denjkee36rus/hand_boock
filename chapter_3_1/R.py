57f0914dc486c2b203327702cff3d5c34e6b3428
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-04-19 14:49:27 +0100

    3.1. Строки, кортежи, списки
    Проверена: `R`

--- chapter_3_1/R.py
+++ chapter_3_1/R.py
@@ -1,13 +1,3 @@
-"""
-0100011100
-Берем символ смотрим следующий символ (справа):
-
-Если следующий символ равен текущему, то увеличиваем счетчик
-Иначе: выведем текущий элемент и его кол-во. Обнуляем.
-"""
-
-
-
 example = input()
 temp_count = 1
 for i in range(len(example) - 1):
@@ -18,5 +8,18 @@ for i in range(len(example) - 1):
         temp_count = 1
 print(example[-1], temp_count)
 
+# Option 2
+line = input()
 
-# TODO: ДЗ
+length: int = len(line)
+count: int = 1
+
+for i in range(length):
+    if i == length - 1:
+        print(line[i], count)
+        break
+    if line[i] == line[i + 1]:
+        count += 1
+    else:
+        print(line[i], count)
+        count = 1
