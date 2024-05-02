51357aa0cfb0e6a7a5b88032926c7209d8be36be
dchekalin <chekalkin_92@mail.ru>
2024-05-02 23:00:14 +0300

    L M

--- chapter_3_2/L.py
+++ chapter_3_2/L.py
@@ -1,1 +1,24 @@
 # TODO: ДЗ
+mans = int(input())
+
+employees = {}
+for i in range(mans):
+    name = input()
+    employees[name] = employees.get(name, 0) + 1
+final_employees = []
+for key, value in employees.items():
+    if value > 1:
+        final_employees.append((key, '-', value))
+final_employees.sort()
+if final_employees:
+    for employee in final_employees: # Не совсем понимаю этот кусок мешали скобки.
+        print(*employee, sep=' ')
+else:
+    print('Однофамильцев нет')
+
+
+
+
+
+
+
--- chapter_3_2/M.py
+++ chapter_3_2/M.py
@@ -1,1 +1,35 @@
 # TODO: ДЗ
+
+count_dishes = int(input())
+spisok_dishes = {}
+for _ in range(count_dishes):
+    name_dishes = input()
+    spisok_dishes[name_dishes] = 0
+
+count_day = int(input())
+
+for _ in range(count_day):
+    count_dishes_2 = int(input())
+    for d in range(count_dishes_2):
+        meet = input()
+        spisok_dishes[meet] = spisok_dishes.get(meet, 0) + 1
+final_spisok = []
+for key, value in spisok_dishes.items():
+    if value == 0:
+        final_spisok.append(key)
+final_spisok.sort()
+if final_spisok:
+    print(*final_spisok, sep='\n')
+else:
+    print('Готовить нечего')
+
+
+
+
+
+
+
+
+
+
+
