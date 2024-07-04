ba7f52ac5b2d549b268365bb044fd6ee82ea5088
Denis Chekalkin <chekalkin_92@mail.ru>
2024-07-05 00:14:18 +0300

    декораторы

--- oop/tasks/alphanber.py
+++ oop/tasks/alphanber.py
@@ -1,1 +1,31 @@
-# https://smartiqa.ru/courses/python/lesson-6#hometask
+import string
+
+class Alphabet:
+    def __init__(self, lang, letters):
+        self.lang = lang
+        self.letters = letters
+
+    def print_letters(self):
+        print("Буквы алфавита:", self.letters)
+
+    def letters_num(self):
+        return len(self.letters)
+
+# Создаем объект класса Alphabet
+alphabet = Alphabet('Русский', 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+
+# Выводим буквы алфавита
+alphabet.print_letters()
+
+# Выводим количество букв в алфавите
+print("Количество букв в алфавите:", alphabet.letters_num())
+
+
+class EngAlphabet(Alphabet):
+    def __init__(self):
+        super().__init__("En", string.ascii_letters)
+        self.lang = "En"
+        self.letters = string.ascii_letters
+
+
+
