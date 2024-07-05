87a6ac5e7d9ab31b46e1dfeebec67759f588bf30
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-07-05 15:20:28 +0100

    5. ООП
    - Разбор задачи: "Алфавит"
    - ДЗ: "Покупка дома"

--- oop/tasks/alphanber.py
+++ oop/tasks/alphanber.py
@@ -1,43 +1,55 @@
 import string
 
+
 class Alphabet:
     def __init__(self, lang, letters):
         self.lang = lang
         self.letters = letters
 
-    def print_letters(self):
-        print("Буквы алфавита:", self.letters)
+    def print(self):
+        print('Буквы алфавита:', self.letters)
 
     def letters_num(self):
         return len(self.letters)
 
-# Создаем объект класса Alphabet
-alphabet = Alphabet('Русский', 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя')
-
-# Выводим буквы алфавита
-alphabet.print_letters()
-
-# Выводим количество букв в алфавите
-print("Количество букв в алфавите:", alphabet.letters_num())
-
 
 class EngAlphabet(Alphabet):
-    __letters_num = 26  # Приватное статическое свойство, хранящее количество букв в алфавите
+    __letters_num: int = 26  # Приватное статическое свойство, хранящее количество букв в алфавите
+
     def __init__(self):
-        super().__init__("En", string.ascii_letters)
-        self.lang = "En"
-        self.letters = string.ascii_letters
-    def is_en_letter(self, letter):
-        if letter in string.ascii_uppercase:
-            print("Символ является заглавной буквой английского  алфавита")
-        else:
-            print("Символ не является заглавной буквой английского  алфавита")
+        super().__init__('En', string.ascii_lowercase)
+
+    def is_en_letter(self, letter: str) -> bool:
+        return letter.lower() in self.letters
 
     def letters_num(self):
         return self.__letters_num
 
+    @staticmethod
+    def example():
+        return 'the quick brown fox jumped over the lazy dog'
 
 
-test_1 = EngAlphabet()
-test_1.is_en_letter("В")
-print()
+# Создаем объект класса Alphabet
+alphabet = Alphabet(
+    'Русский',
+    'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+)
+
+# Выводим буквы алфавита
+alphabet.print()
+
+# Выводим количество букв в алфавите
+print(
+    'Количество букв в алфавите:',
+    alphabet.letters_num()
+)
+
+english = EngAlphabet()
+english.letters_num()
+EngAlphabet.example()
+english.example()
+
+print(english.lang)
+print(english.letters)
+print(english.is_en_letter('g'))
