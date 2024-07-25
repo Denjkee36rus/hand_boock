b04f7c3141034b5a0dd3b8d409cebf5069e233b4
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-07-25 18:55:09 +0100

    Проверка ДЗ по ООП
    - Раздел 1.4: №9

--- oop/OOP Sergei Balakirev/self_1_4/file_9.py
+++ oop/OOP Sergei Balakirev/self_1_4/file_9.py
@@ -1,16 +1,33 @@
 import sys
 
-lst_n = list(map(str.strip, sys.stdin.readline()))
-
 
 class DataBase:
-    lst_data = []
-    FIELDS = ('id', 'name', 'old', 'salary')
+    lst_data: list[dict[str, str]] = []
+    FIELDS: tuple[str, ...] = ('id', 'name', 'old', 'salary')
 
-    def insert(self, data):
+    def select(self, a: int, b: int) -> list:
+        return self.lst_data[a:b + 1]
+
+    def insert(self, data: list[str]) -> None:
+        # TODO: Здесь нужно исправить добавление записей,
+        #  в параметр data приходит следующий список:
+        #  ['1 Sergey 35 120000', '2 Fedor 23 12000', '3 Ivan 13 1200'].
+        #  Нужно улучшить решение.
+
         self.lst_data.append(dict(zip(self.FIELDS, data)))
 
 
-a = DataBase()
-a.insert(lst_n)
-print(a.lst_data)
+if __name__ == '__main__':
+    # INFO: Здесь опечатались, метод readlines() должен быть
+    lst_n: list[str] = list(map(str.strip, sys.stdin.readlines()))
+    # print(lst_n)  # DEBUG
+
+    a = DataBase()
+    a.insert(lst_n)
+    print(a.lst_data)
+
+"""
+1 Sergey 35 120000
+2 Fedor 23 12000
+3 Ivan 13 1200
+"""
