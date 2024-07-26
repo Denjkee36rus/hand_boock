3710c5f0465cf69b6fb60fe33c998bd6375d47ee
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-07-26 13:32:19 +0100

    Проверка ДЗ по ООП
    - Раздел 1.4: №9

--- oop/OOP Sergei Balakirev/self_1_4/file_9.py
+++ oop/OOP Sergei Balakirev/self_1_4/file_9.py
@@ -1,10 +1,11 @@
 import sys
 
 lst_n: list[str] = list(map(str.strip, sys.stdin.readlines()))
+
+
 class DataBase:
     lst_data: list[dict[str, str]] = []
     FIELDS: tuple[str, ...] = ('id', 'name', 'old', 'salary')
-
 
     def insert(self, data: list[str]) -> None:
         # TODO: Здесь нужно исправить добавление записей,
@@ -15,28 +16,23 @@ class DataBase:
             item = item.split()
             self.lst_data.append(dict(zip(self.FIELDS, item)))
 
+    # TODO: 1. Этот метод неверно реализован.
     def select(self, a, b):
         result = []
         for row in self.lst_data[a:b + 1]:
             result.append(' '.join(row.values()))
-        print(*result, sep = '\n')
-
-
-
-
-
+        print(*result, sep='\n')
 
 
 # if __name__ == '__main__':
 #     # INFO: Здесь опечатались, метод readlines() должен быть
 #     lst_n: list[str] = list(map(str.strip, sys.stdin.readlines()))
 #     pass
-    # print(lst_n)  # DEBUG
+# print(lst_n)  # DEBUG
 
 a = DataBase()
 a.insert(lst_n)
-a.select(0,7)
-
+a.select(0, 7)  # TODO: 2. Как исправите метод, здесь нужно добавить print
 
 """
 1 Sergey 35 120000
