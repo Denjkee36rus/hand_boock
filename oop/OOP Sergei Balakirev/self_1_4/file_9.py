48fb5223da8a1450ba37605891024ab343fb7cae
Denis Chekalkin <chekalkin_92@mail.ru>
2024-07-26 15:18:19 +0300

    декораторы

--- oop/OOP Sergei Balakirev/self_1_4/file_9.py
+++ oop/OOP Sergei Balakirev/self_1_4/file_9.py
@@ -1,30 +1,42 @@
 import sys
 
-
+lst_n: list[str] = list(map(str.strip, sys.stdin.readlines()))
 class DataBase:
     lst_data: list[dict[str, str]] = []
     FIELDS: tuple[str, ...] = ('id', 'name', 'old', 'salary')
 
-    def select(self, a: int, b: int) -> list:
-        return self.lst_data[a:b + 1]
 
     def insert(self, data: list[str]) -> None:
         # TODO: Здесь нужно исправить добавление записей,
         #  в параметр data приходит следующий список:
         #  ['1 Sergey 35 120000', '2 Fedor 23 12000', '3 Ivan 13 1200'].
         #  Нужно улучшить решение.
+        for item in data:
+            item = item.split()
+            self.lst_data.append(dict(zip(self.FIELDS, item)))
 
-        self.lst_data.append(dict(zip(self.FIELDS, data)))
+    def select(self, a, b):
+        result = []
+        for row in self.lst_data[a:b + 1]:
+            result.append(' '.join(row.values()))
+        print(*result, sep = '\n')
 
 
-if __name__ == '__main__':
-    # INFO: Здесь опечатались, метод readlines() должен быть
-    lst_n: list[str] = list(map(str.strip, sys.stdin.readlines()))
+
+
+
+
+
+# if __name__ == '__main__':
+#     # INFO: Здесь опечатались, метод readlines() должен быть
+#     lst_n: list[str] = list(map(str.strip, sys.stdin.readlines()))
+#     pass
     # print(lst_n)  # DEBUG
 
-    a = DataBase()
-    a.insert(lst_n)
-    print(a.lst_data)
+a = DataBase()
+a.insert(lst_n)
+a.select(0,7)
+
 
 """
 1 Sergey 35 120000
