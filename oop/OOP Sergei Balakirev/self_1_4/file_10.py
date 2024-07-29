c969d44b299b36d8b8e317584904f2f657d68c46
Denis Chekalkin <chekalkin_92@mail.ru>
2024-07-29 17:58:51 +0300

    декораторы

--- oop/OOP Sergei Balakirev/self_1_4/file_10.py
+++ oop/OOP Sergei Balakirev/self_1_4/file_10.py
@@ -1,20 +1,32 @@
 class Translator:
 
     def add(self, eng, rus):
-        new_dict = []
-        for _ in new_dict:
-            if eng not in  new_dict:
-                new_dict.append(dict(zip(eng.split(), rus.split())))
-            elif
+        new_dict = {}
+        if eng not in new_dict:
+                new_dict[eng] = [rus]
+        elif new_dict[eng, rus] in new_dict:
+                pass
+        else:
+                new_dict.setdefault(eng, []).append(rus)
+        print(new_dict)
 
 
 
 
-
-
-
-        return new_dict
+        # for row in new_dict:
+        #     if eng not in  new_dict:
+        #         new_dict.append(dict(zip(eng.split(), rus.split())))
+        #
+        #     elif eng[rus]
+        #
+        #     else:
+        #         new_dict
+        #
+        #
+        #
+        # return new_dict
 
 a = Translator()
 a.add('Hellow', 'привет')
+a.add('Hellow', 'салам')
 
