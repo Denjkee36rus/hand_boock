ce980b227a08ff637b4aeab1f69d2f62600d4f60
Denis Chekalkin <chekalkin_92@mail.ru>
2024-07-29 17:06:25 +0300

    декораторы

---
+++ oop/OOP Sergei Balakirev/self_1_4/file_10.py
@@ -0,0 +1,20 @@
+class Translator:
+
+    def add(self, eng, rus):
+        new_dict = []
+        for _ in new_dict:
+            if eng not in  new_dict:
+                new_dict.append(dict(zip(eng.split(), rus.split())))
+            elif
+
+
+
+
+
+
+
+        return new_dict
+
+a = Translator()
+a.add('Hellow', 'привет')
+
