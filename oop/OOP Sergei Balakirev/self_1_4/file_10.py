eaf69d10462af9c35704358548121be67f63e08b
Denis Chekalkin <chekalkin_92@mail.ru>
2024-07-29 22:05:11 +0300

    Подвиг_10

--- oop/OOP Sergei Balakirev/self_1_4/file_10.py
+++ oop/OOP Sergei Balakirev/self_1_4/file_10.py
@@ -3,46 +3,30 @@ class Translator:
     def add(self, eng, rus):
         self.new_dict = {}
         if eng not in self.new_dict:
-            self.new_dict[eng] = [rus]
+            self.new_dict.setdefault(eng, rus)
         elif self.new_dict[eng, rus] in self.new_dict:
             pass
         else:
             self.new_dict.setdefault(eng, []).append(rus)
-
+        return self.new_dict
 
     def remove(self, eng):
-        self.new_dict.pop(eng)
+        return self.new_dict.pop(eng)
 
     def traslate(self, eng):
         print(*self.new_dict.get(eng,[]))
         #??
 
-
-
-
-
-
-
-
-
-
-
-        # for row in new_dict:
-        #     if eng not in  new_dict:
-        #         new_dict.append(dict(zip(eng.split(), rus.split())))
-        #
-        #     elif eng[rus]
-        #
-        #     else:
-        #         new_dict
-        #
-        #
-        #
-        # return new_dict
-
 a = Translator()
-a.add('Hellow', 'привет')
-a.add('Hellow', 'салам')
 a.add('tree', 'дерево')
-a.traslate('tree')
+a.add('car', 'машина')
+a.add('car', 'автомобиль')
+a.add('leaf', 'лист')
+a.add('river', 'река')
+a.add('go', 'идти')
+a.add('go', 'ехать')
+a.add('go', 'ходить')
+a.add('milk', 'молоко')
+print(a.__dict__)
+
 
