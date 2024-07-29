22281fbb5743bb2dc4d6bcb5f3e5d62dff839d88
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-07-29 21:22:30 +0100

    Проверка ДЗ по ООП
    - Раздел 1.4: №10

--- oop/OOP Sergei Balakirev/self_1_4/file_10.py
+++ oop/OOP Sergei Balakirev/self_1_4/file_10.py
@@ -1,32 +1,51 @@
+from pprint import pprint
+
+
 class Translator:
-    new_dict = {}
+    __new_dict: dict[str, list[str]] = {}
 
-    def add(self, eng, rus):
-        if eng not in self.new_dict:
-            self.new_dict.setdefault(eng, rus)
-        elif self.new_dict[eng, rus] in self.new_dict:
-            pass
-        else:
-            self.new_dict.setdefault(eng, []).append(rus)
-        print(self.new_dict)
+    def add(self, eng: str, rus: str) -> None:
+        eng, rus = eng.lower(), rus.lower()
+        value: list[str] = self.__new_dict.get(eng, [])
+        if rus not in value:
+            # current_data.append(rus)
+            self.__new_dict[eng] = value + [rus]
 
-    def remove(self, eng):
-        return self.new_dict.pop(eng)
+    # --skip
+    def add_extend(self, eng: str, rus: list[str]) -> None:
+        eng = eng.lower()
+        rus = [word.lower() for word in rus]
+        value: list[str] = self.__new_dict.get(eng, [])
 
-    def traslate(self, eng):
-        print(*self.new_dict.get(eng,[]))
-        #??
+        diff: list[str] = list(set(rus).difference(value))
+        self.__new_dict[eng] = value + diff
 
-a = Translator()
-a.add('tree', 'дерево')
-a.add('car', 'машина')
-a.add('car', 'автомобиль')
-a.add('leaf', 'лист')
-a.add('river', 'река')
-a.add('go', 'идти')
-a.add('go', 'ехать')
-a.add('go', 'ходить')
-a.add('milk', 'молоко')
-print(a.__dict__)
+    def remove(self, eng: str) -> list[str]:
+        return self.__new_dict.pop(eng.lower())
+
+    def translate(self, eng: str) -> str:
+        return ' '.join(self.__new_dict.get(eng.lower(), []))
+
+    # --skip
+    def get_all_words(self) -> None:
+        pprint(self.__new_dict)
 
 
+if __name__ == '__main__':
+    a: Translator = Translator()
+    a.add('tree', 'дерево')
+    a.add('car', 'машина')
+    a.add('car', 'автомобиль')
+    a.add('leaf', 'лист')
+    a.add('river', 'река')
+    a.add('go', 'идти')
+    a.add('go', 'ехать')
+    a.add('go', 'ходить')
+    a.add('milk', 'молоко')
+    a.add_extend('go', ["abba", "abcd", "abba"])
+    a.add_extend('go', ["def", "if", "for", "dEf", "DEf"])
+
+    # ---------------------
+    a.remove('car')
+    print(a.translate('go'))
+    a.get_all_words()
