f22707330ef17859221d2d9ec6ab599f6ea3dd21
Denis Chekalkin <chekalkin_92@mail.ru>
2024-08-05 20:55:23 +0300

    Подвиг_10
    Signed-off-by: Denis Chekalkin <chekalkin_92@mail.ru>

--- oop/OOP Sergei Balakirev/self_1_5/file_6.py
+++ oop/OOP Sergei Balakirev/self_1_5/file_6.py
@@ -1,29 +1,30 @@
-data_graph = list(map(int, input.split()))
+# data_graph = list(map(int, input.split()))
 class Graph:
     def __init__(self, data):
         self.data = data.copy()  # Создаем копию переданного списка
         self.is_show = True  # По умолчанию данные графика показаны
+
+    def get_data(self):
+        return " ".join(map(str, self.data))
 
     def set_data(self, data):
         self.data = data.copy()
 
     def show_table(self):
         if self.is_show:
-            print(" ".join(map(str, self.data)))
+            print(self.get_data())
         else:
             print("Отображение данных закрыто")
 
     def show_graph(self):
-        result = " ".join(map(str, self.data))
         if self.is_show:
-            print(f"Графическое отображение данных: {result}")
+            print(f"Графическое отображение данных: {self.get_data()}")
         else:
             print("Отображение данных закрыто")
 
     def show_bar(self):
-        result = " ".join(map(str, self.data))
         if self.is_show:
-            print(f'Столбчатая диаграмма: {result}')
+            print(f'Столбчатая диаграмма: {self.get_data()}')
         else:
             print("Отображение данных закрыто")
 
@@ -31,6 +32,7 @@ class Graph:
         self.is_show = fl_show
 
 
+data_graph = [8, 11, 10, -32, 0, 7, 18]
 
 gr_1 = Graph(data_graph)
 
