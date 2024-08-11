2d67c81c42caa6f2496413fd1a1781ea88d0c690
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-08-11 14:02:57 +0100

    Проверка ДЗ по ООП
    - Раздел 1.5: №7 и №8

--- oop/OOP Sergei Balakirev/self_1_5/file_7.py
+++ oop/OOP Sergei Balakirev/self_1_5/file_7.py
@@ -1,31 +1,50 @@
-class CPU: # класс для описания процессов
+class CPU:
+    """Класс для описания процессов."""
+
     def __init__(self, name, fr):
         self.name = name
         self.fr = fr
 
 
-class Memory: # Memory. - класс для описания памяти;
+class Memory:
+    """Класс для описания оперативной памяти."""
+
     def __init__(self, name, volume):
         self.name = name
         self.volume = volume
 
 
-class MotherBoard: # MotherBoard - класс для описания материнских плат.
-    def __init__(self, name, cpu, *mems):
+class MotherBoard:
+    """Класс для описания материнских плат."""
+
+    def __init__(self, name: str, cpu: CPU, *mems):
         self.name = name
         self.cpu = cpu
-        self.total_mem_slots = 4
-        self.mem_slots = mems[:self.total_mem_slots]
+        self.__total_mem_slots: int = 4
+        self.mem_slots: tuple[Memory] = mems[:self.__total_mem_slots]
 
     def get_config(self):
         result = []
         result.append(f'Материнская плата: {self.name}')
         result.append(f'Центральный процессор: {self.cpu.name} {self.cpu.fr}')
-        result.append(f'Слотов памяти: {self.total_mem_slots}')
-        result.append(f"Память:" + '; '.join(lambda x)
+        result.append(f'Слотов памяти: {self.__total_mem_slots}')
+        result.append(
+            f'Память: ' + '; '.join(
+                f'{mem.name} - {mem.volume}'
+                for mem in self.mem_slots
+            )
+        )
+        return result
 
 
+if __name__ == '__main__':
+    RAM: int = 8
+    GHZ: float = 3.4
+    n: int = 2
 
+    # -----------------------
 
-
-
+    mem_obj: list[Memory] = [Memory(f'Память {i}', RAM) for i in range(1, n)]
+    cpu_obj: CPU = CPU('Apple Silicon M3 Pro', GHZ)
+    mb: MotherBoard = MotherBoard('Apple', cpu_obj, *mem_obj)
+    print(mb.get_config())
--- oop/OOP Sergei Balakirev/self_1_5/file_8.py
+++ oop/OOP Sergei Balakirev/self_1_5/file_8.py
@@ -1,47 +1,47 @@
-
 class Cart:
     def __init__(self):
-        self.goods = []
+        self.goods: list['Good'] = []
 
-    def add(self, gd):
-        self.gd = gd
-        self.goods.append(self.gd)
+    def add(self, gd: 'Good'):
+        self.goods.append(gd)
 
     def remove(self, indx: int):
+        # TODO: Что если список товаров пуст?
         del self.goods[indx]
 
     def get_list(self) -> list[str]:
-        result = [f'{gd.name}:{gd.price}' for gd in self.goods]
-        print(result)
+        result = [f'{gd.name}: {gd.price}' for gd in self.goods]
         return result
 
-class Table:
+
+class Good:
+    """Базовый класс для товаров."""
+
     def __init__(self, name: str, price: int):
         self.name = name
         self.price = price
 
 
-class TV:
-    def __init__(self, name: str, price: int):
-        self.name = name
-        self.price = price
-
-class Notebook:
-    def __init__(self, name: str, price: int):
-        self.name = name
-        self.price = price
-
-class Cup:
-    def __init__(self, name: str, price: int):
-        self.name = name
-        self.price = price
-
-cart = Cart()
-
-cart.add(TV('samsung', 20000))
-cart.add(TV('LG', 20000))
-
-cart.get_list()
+class Table(Good):
+    pass
 
 
+class TV(Good):
+    pass
 
+
+class Notebook(Good):
+    pass
+
+
+class Cup(Good):
+    pass
+
+
+if __name__ == '__main__':
+    cart = Cart()
+
+    cart.add(TV('samsung', 20000))
+    cart.add(TV('LG', 20000))
+
+    print(cart.get_list())
